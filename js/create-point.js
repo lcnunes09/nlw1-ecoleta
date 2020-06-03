f018ca97c47ef82133feaa757e83b6b835dc8443
Luciana Nunes <laluciananunes@MacBook-Pro-de-Luciana.local>
2020-06-03 20:31:58 -0300

    Corrigindo bug da seleção da cidade e finalizando o cadastro de coleta

--- js/create-point.js
+++ js/create-point.js
@@ -23,11 +23,14 @@ function getCities(event){
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"        
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for (const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -37,3 +40,35 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => item === itemId)
+
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+    
+}
+
