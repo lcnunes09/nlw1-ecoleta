478cee7327af0f0179c9c66738a76644e2c40ed2
Luciana Nunes <lcnunes09@gmail.com>
2020-06-05 22:41:17 -0300

    Organizando pastas, criando servidor node, implementando templates com nunjucks e deixando create-point responsivo

---
+++ src/server.js
@@ -0,0 +1,33 @@
+// npm init -y
+// npm install express
+// npm install nodemon -D
+// npm install nunjucks
+
+// Quando baixar o projeto: 
+// npm install
+// npm start
+
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+server.listen(3000)
