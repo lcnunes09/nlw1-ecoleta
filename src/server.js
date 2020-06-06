5a439974915c1deaee4a24d8e64258d620258811
Luciana Nunes <lcnunes09@gmail.com>
2020-06-05 23:54:57 -0300

    Implementando consulta no banco de dados na página de pesquisa

--- src/server.js
+++ src/server.js
@@ -30,7 +30,16 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html")
+    db.all(`SELECT * FROM places`, function(err, rows){
+        if (err){
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return res.render("search-results.html", { places: rows, total })
+    })
+
 })
 
 server.listen(3000)
